415e981fa819a7535f4bb9521816845007329c62
Islamwael-1999 <islam.wael1999@gmail.com>
2022-05-08 16:22:51 +0200

    Post method added

--- client.py
+++ client.py
@@ -1,27 +1,102 @@
-
-import socket			
+from curses import raw
+import socket
 import struct
 import sys
-def initiateConnection():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 80	
-    try:		
-        s.connect(('127.0.0.1', port))
-        return s
-    except:
-        print("Connection failed")
-        sys.exit(0)		
+import os
 
-def makePacket(fileName,Method):
-    msg=Method+" /"+fileName+" HTTP/1.0 \nHost: reqbin.com"
-    stringSize=len(msg)
-    packet=struct.pack(str(stringSize)+'s',msg.encode("utf-8"))
-    return packet
+class Client:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = 85
+        try:
+            self.s.connect(('127.0.0.1', port))
+        except:
+            print("Connection failed")
+            sys.exit(0)
 
-clientSocket=initiateConnection()
-fileName=input("Please enter the file u wish to get")
-packet=makePacket(fileName,"GET")
+    def makeGetPacket(self, fileName):
+        msg = "GET /"+fileName+" HTTP/1.0\nHost: reqbin.com"
+        stringSize = len(msg)
+        packet = struct.pack(str(stringSize)+'s', msg.encode("utf-8"))
+        return packet
+    
+    def makePostPacket(self,fileName):
+        with open(fileName) as f:
+                lines = f.readlines()
+                f.seek(0, os.SEEK_END)
+                fileSize=f.tell()
+                f.close()
+        msg = "POST /"+fileName+" HTTP/1.0\nHost: reqbin.com\nContent-Length:"+str(fileSize)
+        stringSize = len(msg)
+        packet = struct.pack(str(stringSize)+'s', msg.encode("utf-8"))
+        return packet
 
-clientSocket.send(packet)
-clientSocket.close()	
-	
+    def sendState(self):
+        try:
+            while True:
+                method = input("Enter type of request POST or GET :")
+                if method == 'GET':
+                    fileName = input("Please enter the file u wish to get: ")
+                    packet = self.makeGetPacket(fileName)
+                    self.s.send(packet)
+                    self.receiveState(method)
+                elif method=='POST':
+                    fileName = input("Please enter the file u wish to POST: ")
+                    try:
+                        packet = self.makePostPacket(fileName)
+                    except :
+                        print('File not Found')
+                        continue
+                    self.s.send(packet)
+                    self.sendFile(fileName)
+                    self.receiveState(method)
+        except KeyboardInterrupt:
+            self.s.close()
+    def sendFile(self,fileName):
+        with open(fileName) as f:
+                lines = f.readlines()
+                for line in lines:
+                    # print(line)
+                    stringSize = len(line)
+                    packet = struct.pack(
+                        str(stringSize)+'s', line.encode("utf-8"))
+                    self.s.send(packet)
+        return
+    def receiveState(self,method):
+        if method == 'GET':
+            packet = self.s.recv(28)
+            rawMsg = struct.unpack('8s i 9s i', packet)
+            Responsemsg = rawMsg[1]
+            header = rawMsg[0].decode('utf-8')+' '+str(rawMsg[1])+' '+rawMsg[2].decode('utf-8')
+            print(header)
+            if Responsemsg == 200:
+                payloadSize = rawMsg[3]
+                f = open("receivedFile.txt", 'a')
+                while payloadSize != 0:
+                    if payloadSize > 1000:
+                        data = self.s.recv(1000)
+                        payloadSize -= 1000
+
+                    elif payloadSize > 100:
+                        data = self.s.recv(100)
+                        payloadSize -= 100
+                    elif payloadSize > 10:
+                        data = self.s.recv(10)
+                        payloadSize -= 10
+                    else:
+                        data = self.s.recv(payloadSize)
+                        payloadSize = 0
+                    f.write(data.decode('utf-8'))
+                    # print(data.decode('utf-8'))
+                f.close()
+        elif method =='POST':
+            packet = self.s.recv(24)
+            rawMsg = struct.unpack('8s i 9s', packet)
+            Responsemsg = rawMsg[1]
+            header = rawMsg[0].decode('utf-8')+' '+str(rawMsg[1])+' '+rawMsg[2].decode('utf-8')
+            print(header)
+        return
+
+
+client = Client()
+client.sendState()
--- server.py
+++ server.py
@@ -1,35 +1,103 @@
-
-import socket			
+import socket
 import struct
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		
-print ("Socket successfully created")
-
-port = 80			
+import os
 
 
-s.bind(('', port))		
-print ("socket binded to %s" %(port))
+class Server:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket successfully created")
+        port = 85
+        self.sock.bind(('', port))
+        print("socket binded to %s" % (port))
+        self.sock.listen(5)
+        print("socket is listening")
+
+    def receiveState(self):
+        try:
+            self.c, addr = self.sock.accept()
+            while True:
+                packet = self.c.recv(200)
+                if not packet:
+                    self.c.close()
+                    break
+                msg = struct.unpack(str(len(packet))+'s', packet)
+                msg = msg[0].decode("utf-8")
+                self.respondState(msg)
+        except KeyboardInterrupt:
+            self.c.close()
+
+    def respondState(self, msg):
+        data = msg.split(' ')
+        if data[0] == 'GET':
+            self.getMethod(data)
+            # print('Get')
+        if data[0] == 'POST':
+            for d in data:
+                print(d)
+            self.postMethod(data)
+
+    def getMethod(self, Msgdata):
+        filename = Msgdata[1]
+        tokens = filename.split('/')
+        filename = tokens[1]
+        print(filename)
+        try:
+            with open(filename) as f:
+                lines = f.readlines()
+                f.seek(0, os.SEEK_END)
+                fileSize = f.tell()
+                acceptMsg = 200  # Message Accepted
+                packet = struct.pack(
+                    '8s i 9s i', b'HTTP/1.0', acceptMsg, b'OK       ', fileSize)
+                self.c.send(packet)
+                for line in lines:
+                    print(line)
+                    stringSize = len(line)
+                    packet = struct.pack(
+                        str(stringSize)+'s', line.encode("utf-8"))
+                    self.c.send(packet)
+                return
+        except:
+            msg = 404
+            packet = struct.pack('8s i 9s i', b'HTTP/1.0',
+                                 msg, b'NOT FOUND', 0)
+            self.c.send(packet)
+            return
+
+    def postMethod(self, MsgData):
+        filename = MsgData[1]
+        tokens = filename.split('/')
+        filename = 'f'+tokens[1]
+        print(filename)
+        try:
+            f= open(filename, 'w')
+            while payloadSize != 0:
+                if payloadSize > 1000:
+                    data = self.c.recv(1000)
+                    payloadSize -= 1000
+
+                elif payloadSize > 100:
+                    data = self.c.recv(100)
+                    payloadSize -= 100
+                elif payloadSize > 10:
+                    data = self.c.recv(10)
+                    payloadSize -= 10
+                else:
+                    data = self.c.recv(payloadSize)
+                    payloadSize = 0
+                f.write(data.decode('utf-8'))
+            f.close()
+            msg = 200
+            packet = struct.pack(
+                '8s i 9s', b'HTTP/1.0', msg, b'OK       ')
+            self.c.send(packet)
+        except:
+            msg = 404
+            packet = struct.pack('8s i 9s', b'HTTP/1.0', msg, b'NOT FOUND')
+            self.c.send(packet)
+            return
 
 
-s.listen(5)	
-print ("socket is listening")		
-
-
-while True:
-
-
-    c, addr = s.accept()	
-    print ('Got connection from', addr )
-
-    packet =c.recv(200)
-    
-    msg=struct.unpack(str(len(packet))+'s',packet)
-
-    print(msg[0].decode("utf-8"))
-
-    
-    c.close()
-
-    
-    break
+server = Server()
+server.receiveState()
